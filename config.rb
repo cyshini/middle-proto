684536770ed70e2b5914b368c381484cd841e685
cyshini <francois.vilaire@gmail.com>
2015-03-13 18:27:13 +0100

    Basic config.rb for deployment, and new directories for assets and layouts

--- config.rb
+++ config.rb
@@ -2,43 +2,20 @@
 # Blog settings
 ###
 
-# Time.zone = "UTC"
+Time.zone = "Paris"
 
 activate :blog do |blog|
-  # This will add a prefix to all links, template references and source paths
-  # blog.prefix = "blog"
-
-  # blog.permalink = "{year}/{month}/{day}/{title}.html"
-  # Matcher for blog source files
-  # blog.sources = "{year}-{month}-{day}-{title}.html"
-  # blog.taglink = "tags/{tag}.html"
-  # blog.layout = "layout"
-  # blog.summary_separator = /(READMORE)/
-  # blog.summary_length = 250
-  # blog.year_link = "{year}.html"
-  # blog.month_link = "{year}/{month}.html"
-  # blog.day_link = "{year}/{month}/{day}.html"
-  # blog.default_extension = ".markdown"
-
+  blog.name = "blog"
+  blog.permalink = "blog/:title.html"
+  blog.sources = "blog/articles/:title.html"
+  blog.layout = "layouts/blog_layout"
+  blog.default_extension = ".markdown"
+  blog.paginate = false
   blog.tag_template = "tag.html"
   blog.calendar_template = "calendar.html"
-
-  # Enable pagination
-  # blog.paginate = true
-  # blog.per_page = 10
-  # blog.page_link = "page/{num}"
 end
 
 page "/feed.xml", layout: false
-
-###
-# Compass
-###
-
-# Change Compass configuration
-# compass_config do |config|
-#   config.output_style = :compact
-# end
 
 ###
 # Page options, layouts, aliases and proxies
@@ -71,27 +48,17 @@ page "/feed.xml", layout: false
 # Reload the browser automatically whenever files change
 activate :livereload
 
-# Methods defined in the helpers block are available in templates
-# helpers do
-#   def some_helper
-#     "Helping"
-#   end
-# end
-
-set :css_dir, 'stylesheets'
-
-set :js_dir, 'javascripts'
-
-set :images_dir, 'images'
+set :js_dir, 'assets/javascripts'
+set :css_dir, 'assets/stylesheets'
+set :images_dir, 'assets/images'
 
 # Build-specific configuration
 configure :build do
+
   activate :minify_css
   activate :minify_javascript
   activate :asset_hash
-
   activate :relative_assets
   set :relative_links, true
 
-  activate :sitemap, hostname: data.settings.site.url
 end
